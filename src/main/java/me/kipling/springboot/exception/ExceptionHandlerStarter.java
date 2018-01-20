80e1ce704299f7a5c9ccc905096865ed981b2916
KiplingGit <zhangwei.168520@sohu.com>
2018-01-20 21:50:56 +0800

    1

--- src/main/java/me/kipling/springboot/exception/ExceptionHandlerStarter.java
+++ src/main/java/me/kipling/springboot/exception/ExceptionHandlerStarter.java
@@ -3,6 +3,11 @@ package me.kipling.springboot.exception;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1
+ * @author Kipling
+ *
+ */
 @SpringBootApplication
 public class ExceptionHandlerStarter {
 
